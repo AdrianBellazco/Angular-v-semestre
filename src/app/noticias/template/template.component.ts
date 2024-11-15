d9cd2e77d133003b9dbd1257a664a5205976640b
adrian <adrian.saavedra01@uceva.edu.co>
2024-11-14 22:58:37 -0500

    Se ha instalardo switch alert

--- src/app/noticias/template/template.component.ts
+++ src/app/noticias/template/template.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-template',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrl: './template.component.css'
 })
 export class TemplateComponent {
+
+  editar(){
+
+  }
+
+  eliminar(){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Estas seguro",
+      text: "Tu quieres borrar la noticia",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Si, quiero borrarla!",
+      cancelButtonText: "No, cancela!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire({
+          title: "Borrada!",
+          text: "Tu noticia ha sido borrada.",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelada.",
+          text: "No se borro la noticia.",
+          icon: "error"
+        });
+      }
+    });
+  }
 
 }
