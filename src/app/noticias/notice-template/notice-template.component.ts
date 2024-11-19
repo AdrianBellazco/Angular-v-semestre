767c95c05e68b978e0e7d4b7bbd9a41d2bc8cce5
adria <adrian.saavedra01@uceva.edu.co>
2024-11-19 18:48:27 -0500

    Se agrego en editar, se mejoro el eliminar, ya fuciona el enlistar y se le soluciono problemas del cors

--- src/app/noticias/notice-template/notice-template.component.ts
+++ src/app/noticias/notice-template/notice-template.component.ts
@@ -31,37 +31,43 @@ export class NoticeTemplateComponent {
 
 
 
-  async editar() {
+  async editar(noticias: Noticias): Promise<void> {
     const {value: formValues} = await Swal.fire({
       title: "editar noticia",
       html: `
-    <input id="swal-input1" class="swal2-input" placeholder="Id">
-    <input id="swal-input2" class="swal2-input" placeholder="Titulo de noticia">
-    <textarea id="swal-textarea" class="swal2-textarea" placeholder="Detalles"></textarea>
+    <input id="id" class="swal2-input" placeholder="Id" disabled value ="${noticias.id}">
+    <input id="titulo" class="swal2-input" placeholder="Titulo de noticia"  value ="${noticias.titulo}">
+    <textarea id="texto" class="swal2-textarea" placeholder="Detalles"
+     style="width: 90%; height: 150px; resize: none; margin-bottom: 20px; display: block;">${noticias.texto}</textarea>
      <input id="swal-file" type="file" accept="image/*" class="swal2-input" style="display:block; margin: auto;">
   `,
       focusConfirm: false,
       showCancelButton: true,
       confirmButtonColor: '#4CAF50',
       confirmButtonText: 'Guardar cambios',
-
-
       preConfirm: () => {
-        return [];
+        const id = (<HTMLInputElement>Swal.getPopup()!.querySelector('#id')).value;
+        const titulo = (<HTMLInputElement>Swal.getPopup()!.querySelector('#titulo')).value;
+        const texto = (<HTMLInputElement>Swal.getPopup()!.querySelector('#texto')).value;
+        return {id, titulo, texto};
       }
     });
     if (formValues) {
       Swal.fire({
         confirmButtonColor: '#4CAF50',
-        title: "Quieres jugardar los cambios?",
+        title: "¿Quieres guardar los cambios?",
         showDenyButton: true,
         showCancelButton: true,
         confirmButtonText: "Guardar cambios",
         denyButtonText: `No guardar`
       }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-          Swal.fire("Guardados!", "", "success");
+          // Usa formValues directamente porque ahora es un objeto con las propiedades correctas
+          noticias.titulo = formValues.titulo;
+          noticias.texto = formValues.texto;
+          this.noticiaService.updateNoticia(noticias).subscribe(() => {
+            Swal.fire("Guardados!", "", "success");
+          });
         } else if (result.isDenied) {
           Swal.fire("Los cambios no se guardaron", "", "info");
         }
@@ -69,44 +75,29 @@ export class NoticeTemplateComponent {
     }
   }
 
-  eliminar(){
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: "btn btn-success",
-        cancelButton: "btn btn-danger",
-
-      },
-      buttonsStyling: false
-
-    });
-    swalWithBootstrapButtons.fire({
-      title: "Estas seguro",
-      text: "Tu quieres borrar la noticia",
-      icon: "warning",
+  eliminar(id: number): void {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'No podrás revertir esto',
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonText: "Si, quiero borrarla!",
       confirmButtonColor: '#4CAF50',
-      cancelButtonText: "No, cancela!",
-      reverseButtons: true
+      cancelButtonColor: '#c94646',
+      confirmButtonText: 'Sí, eliminar'
     }).then((result) => {
       if (result.isConfirmed) {
-        swalWithBootstrapButtons.fire({
-          title: "Borrada!",
-          text: "Tu noticia ha sido borrada.",
-          icon: "success"
+        this.noticiaService.deleteNotice(id).subscribe(() => {
+          this.cargarNoticias();
+          Swal.fire('¡Eliminado!', 'la noticia ha sido eliminado.', 'success');
+
         });
-      } else if (
-        /* Read more about handling dismissals below */
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-        swalWithBootstrapButtons.fire({
-          title: "Cancelada.",
-          text: "No se borro la noticia.",
-          icon: "error"
-        });
+
       }
     });
   }
 
+  private cargarNoticias(){
+this.noticiaService.getNoticias().subscribe(data => this.noticias = data);
+  }
 
 }
--- src/app/noticias/services/noticia.service.ts
+++ src/app/noticias/services/noticia.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Noticias} from '../models/noticias';
 
@@ -7,12 +7,31 @@ import {Noticias} from '../models/noticias';
   providedIn: 'root'
 })
 export class NoticiaService {
-  private apiUrl = '/noticias/mostrar_noticia';
+  private apiUrl = '/noticias';
 
   constructor(private http: HttpClient) {}
 
   getNoticias(): Observable<Noticias[]> {
-    return this.http.get<Noticias[]>(this.apiUrl);
+    return this.http.get<Noticias[]>(`${this.apiUrl}/mostrar_noticia`);
   }
+
+  //crear noticia
+  createNoticia(noticia: Noticias): Observable<Noticias> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.put<Noticias>(`${this.apiUrl}/crear_noticia`, noticia,{ headers});
+  }
+
+  //eliminar noticia
+  deleteNotice(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/eliminar_noticia/${id}`);
+  }
+
+  //actualizar
+  updateNoticia(noticia: Noticias): Observable<Noticias> {
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.put<Noticias>(`${this.apiUrl}/modificar_noticia/${noticia.id}`, noticia,{ headers});
+
+  }
+
 
 }
